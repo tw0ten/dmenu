b5026fa0982348b67341ffd2ca0351929973eab0
tw0ten <twoten.eq@gmail.com>
2024-10-19 16:37:00 +0300

    -

--- config.def.h
+++ config.def.h
@@ -1,17 +1,18 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"monospace:size=10"
-};
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *fonts[] = { "JetBrainsMono-Regular:size=8", "SymbolsNerdFontMono-Regular:size=6" };
+static const char *prompt = "=";            /* -p  option; prompt to the left of input field */
+static const char col_bg[] = "#202020";
+static const char col_fg[] = "#FFFFFF";
+static const char col_ac[] = "#40E0D0";
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { col_fg, col_bg },
+	[SchemeSel]  = { col_bg, col_ac },
+	[SchemeOut]  = { col_bg, col_fg },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
