a6b1dedb2ac859c88296c2772061d582bf79ab46
tw0ten <twoten.eq@gmail.com>
2024-10-29 19:27:05 +0200

    *

--- config.def.h
+++ config.def.h
@@ -5,17 +5,14 @@ static int topbar = 0;                      /* -b  option; if 0, dmenu appears a
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = { "JetBrainsMono-Regular:size=8", "SymbolsNerdFontMono-Regular:size=6" };
 static const char *prompt = "=";            /* -p  option; prompt to the left of input field */
-static const char col_bg[] = "#202020";
-static const char col_fg[] = "#FFFFFF";
-static const char col_ac[] = "#40E0D0";
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { col_fg, col_bg },
-	[SchemeSel]  = { col_bg, col_ac },
-	[SchemeOut]  = { col_bg, col_fg },
+	/*                fg         bg       */
+	[SchemeNorm] = { "#FFFFFF", "#202020" },
+	[SchemeSel]  = { "#202020", "#40E0D0" },
+	[SchemeOut]  = { "#202020", "#FFFFFF" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines = 0;
 
 /*
  * Characters not considered part of a word while deleting words
